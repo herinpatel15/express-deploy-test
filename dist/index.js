067d2906edee7fbeafb39402e69d4cd4727f16d4
herinpatel15 <herinpatel15113@gmail.com>
2024-12-24 20:47:36 +0530

    abtest build

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.status(200).json({ msg: "hello herin" });
 });
 app.listen(3000, () => {
     console.log('server run 3000');
